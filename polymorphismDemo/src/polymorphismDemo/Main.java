27ea87505541f6b848fd7772d79b81c8c2d0ba2b
ugrasburak <114237134+ugrasburak@users.noreply.github.com>
2022-10-13 13:47:14 +0300

    Add files via upload

---
+++ polymorphismDemo/src/polymorphismDemo/Main.java
@@ -0,0 +1,17 @@
+package polymorphismDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * BaseLogger[] loggers=new BaseLogger[] {new DatabaseLogger(),new
+		 * EmailLogger(),new FileLogger(),new ConsoleLogger()}; for (BaseLogger logger :
+		 * loggers) { logger.log("log mesajı"); }
+		 */
+		
+		CustomerManager customerManager=new CustomerManager(new DatabaseLogger());
+		customerManager.add();
+		
+	}
+
+}
