ac580f8adaae89a31ae27c8f7283120b3902692d
ugrasburak <114237134+ugrasburak@users.noreply.github.com>
2022-10-13 13:46:42 +0300

    Add files via upload

---
+++ inheritance/src/inheritance/PersonManager.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class PersonManager {
+	public void add() {
+		System.out.println("Eklendi");
+	}
+	public void list() {
+		System.out.println("Listelendi");
+	}
+}
---
+++ overriding/src/overriding/BaseKrediManager.java
@@ -0,0 +1,9 @@
+package overriding;
+
+public class BaseKrediManager {
+	public double hesapla(double tutar) {
+		return tutar*1.18;		
+	}
+	//final keyword'ü ile bir metotu override edilemez yapabiliriz
+
+}
